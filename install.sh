a02d7f480d2460c401ad169fc97e22e8dcb03979
Felipe Rocha <f.rocha.felipe@gmaim.com>
2022-10-05 12:54:51 +0200

    small update in the installation script

--- install.sh
+++ install.sh
@@ -3,6 +3,12 @@ TUTORIAL_PATH=/home/ffiguere/ECN/handsOnCISM
 DDFENICS_ENV=ddfenics
 DDFENICS_GIT=https://github.com/felipefr/ddfenics_tutorial.git
 
+# run
+# 1) sh install.sh 1
+# 2) conda activate $DDFENICS_ENV
+# 3) sh install.sh 2
+# 4) Run tutorial with jupyter-lab (optionally --notebook-dir=/home/...)
+
 printf "options:\n 1 - conda environment creation and git clone\n 2 - installing of additional packages\n chosen option: $1\n"
 
 if [ $1 -eq 1 ]; then
@@ -12,13 +18,7 @@ if [ $1 -eq 1 ]; then
 	cp extra_python_folders.pth $CONDA_PATH/envs/$DDFENICS_ENV/lib/python3.8/site-packages/extra_python_folders.pth
 fi 
 
-
-# conda activate $DDFENICS_ENV # Not working
-
 if [ $1 -eq 2 ]; then
 	python -m ipykernel install --user --name=$DDFENICS_ENV
 	pip install gmsh meshio==3.3.1 pygmsh==6.0.2
 fi
-
-# Run tutorial with jupyter-lab (--notebook-dir=/home/...)
-
